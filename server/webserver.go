9aaa10727fce13f2c1912b55043bd2feb09bd3ee
Jim Gilbert <jgilbert@akamai.com>
2020-07-20 14:59:07 -0400

    Add a charting library and simplify the handler

--- server/webserver.go
+++ server/webserver.go
@@ -2,87 +2,27 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"io/ioutil"
 	"net/http"
-	"regexp"
+	"os"
 
-	edgegrid "github.com/akamai/AkamaiOPEN-edgegrid-golang"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/edgegrid"
 	reportsgtm "github.com/akamai/AkamaiOPEN-edgegrid-golang/reportsgtm-v1"
+
+	"github.com/go-echarts/go-echarts/charts"
 )
 
-// Page to read and write
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+func handler(w http.ResponseWriter, _ *http.Request) {
+	nameItems := []string{"A", "B", "C", "D", "E", "F"}
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "Bar"})
+	bar.AddXAxis(nameItems).
+		AddYAxis("A", []int{20, 30, 40, 10, 24, 36}).
+		AddYAxis("B", []int{35, 14, 25, 68, 44, 23})
+	f, err := os.Create("bar.html")
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
 	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
+	bar.Render(w, f)
 }
 
 func main() {
@@ -100,8 +40,6 @@ func main() {
 		}
 	}
 
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", handler)
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
